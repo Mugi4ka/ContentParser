99e7d829e3bb779fb11ac69eae41b57c0781b918
baydikov <baydikov@santehsmart.ru>
2020-09-30 18:18:21 +0300

    fix sql error

--- app/Http/Controllers/ParserController.php
+++ app/Http/Controllers/ParserController.php
@@ -3,12 +3,8 @@
 namespace App\Http\Controllers;
 
 use DiDom\Document;
-use DiDom\Query;
-use DOMDocument;
-use DOMXPath;
-use Illuminate\Database\Console\Migrations\MigrateCommand;
+use ErrorException;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Schema;
@@ -69,34 +65,48 @@ class ParserController extends Controller
     public function parseContent()
     {
         $testPers = DB::table('links')->pluck('link');
-
-        foreach ($testPers as $testPer) {
-
-            $document = new Document('https://belbagno.ru/product/akrilovaya-vanna-belbagno-bb101-120-70/', true);
+        $fl_arrays = preg_grep("/(dushevoy-ugolok|shtorka|dver-v-proyem|dushevaya-peregorodka)/i", $testPers->toArray());
+        foreach ($fl_arrays as $fl_array) {
+            $document = new Document($fl_array, true);
             $posts = $document->find('.product-item-detail-properties-name::text');
             $values = $document->find('.product-item-detail-properties-val::text');
             $valuesVendor = $document->find('.product-item-detail-properties-val > a::text');
-            array_splice($values, 0, 0, $valuesVendor[0]);
-            array_splice($values, 2, 0, $valuesVendor[1]);
-            $posts = array_unique($posts);
-            $values = array_unique($values);
-            $posts = array_map(function($posts) {
-                return trim($posts);
-            }, $posts);
-            $values = array_map(function($values) {
-                return trim($values);
-            }, $values);
-            $ars = array_combine($posts, $values);
-            foreach ($ars as $key => $value) {
-                $key= trim($key);
-                if (!Schema::hasColumn('contents' ,$key)) {
-                    Schema::table('contents', function (Blueprint $table) use ($key) {
-                        $table->text($key)->nullable();
-                    });
+            $description = $document->find('.product-item-detail-preview::text');
+            $description = implode($description);
+            $productName = $document->find('.navigation-title::text');
+            $productName = implode($productName);
+            if ($posts) {
+                try {
+                    array_splice($values, 0, 0, $valuesVendor[0]);
+                    array_splice($values, 2, 0, $valuesVendor[1]);
+                } catch (ErrorException $e) {
+                    continue;
                 }
-            }
-            DB::table('contents')->insert($ars);
+                unset($posts[2]);
+                unset($values[2]);
+//            $posts = array_unique($posts);
+//            $values = array_unique($values);
+                $posts = array_map(function ($posts) {
+                    return trim($posts);
+                }, $posts);
+                $values = array_map(function ($values) {
+                    return trim($values);
+                }, $values);
+                $posts = $name = str_replace('.', '', $posts);
+                $ars = array_combine($posts, $values);
+                $ars += ['Название' => $productName];
+                $ars += ['Описание' => $description];
+                foreach ($ars as $key => $value) {
+                    $key =  trim($key);
+                    if (!Schema::hasColumn('contents', $key)) {
+                        Schema::table('contents', function (Blueprint $table) use ($key) {
+                            $table->text($key)->nullable();
+                        });
+                    }
+                }
 
+                DB::table('contents')->insert($ars);
+            }
         }
     }
 }
